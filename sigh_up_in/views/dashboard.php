c8d4261232a262a07ce57157185c143cb20cc66d
Prabudda Perera <72686198+Katz2003@users.noreply.github.com>
2022-12-27 12:59:31 +0530

    Add files via upload

---
+++ sigh_up_in/views/dashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login Page with Background Image Example</title>
+  <link rel="stylesheet" href="../css/sign-up.css">
+</head>
+<body>
+<!-- partial:index.partial.html -->
+
+<div id="bg"></div>
+
+
+<form  method="POST">
+  <div class="h1">
+      <center><h1>Welcome to Events.lk</h1>
+      <h1>This Is the Dashboard</h3></center>
+  </div>
+    
+
+<!-- <h2 class="hr-lines">or</h2> -->
+
+</form>
+
+
+<!-- partial -->
+  
+</body>
+</html>
